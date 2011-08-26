f731727896a2ac6773d81919ac1b706133d8ec1e
Walker Wilkins <wwilkins@gmail.com>
2011-08-26 01:17:56 +0000

    all done

--- topics/about_prototypal_inheritance.js
+++ topics/about_prototypal_inheritance.js
@@ -6,27 +6,27 @@ module("About Prototypal Inheritance (topics/about_prototypal_inheritance.js)");
 // this 'class' pattern defines a class by its constructor
 var Mammal = function(name) {
     this.name = name;
-}
+};
 // things that don't need to be set in the constructor should be added to the constructor's prototype property.
 Mammal.prototype = {
     sayHi: function() {
         return "Hello, my name is " + this.name;
     }
-}
+};
 
 test("defining a 'class'", function() {
     var eric  = new Mammal("Eric");
-    equals(eric.sayHi(), __, 'what will Eric say?');
+    equals(eric.sayHi(), 'Hello, my name is Eric', 'what will Eric say?');
 });
 
 // add another function to the Mammal 'type' that uses the sayHi function
 Mammal.prototype.favouriteSaying = function() {
     return this.name + "'s favourite saying is " + this.sayHi(); 
-}
+};
 
 test("more functions", function() {
     var bobby = new Mammal("Bobby");
-    equals(bobby.favouriteSaying(), __, "what is Bobby's favourite saying?"); 
+    equals(bobby.favouriteSaying(), "Bobby's favourite saying is Hello, my name is Bobby", "what is Bobby's favourite saying?"); 
 });
 
 test("calling functions added to a prototype after an object was created", function() {
@@ -35,7 +35,7 @@ test("calling functions added to a prototype after an object was created", funct
         return this.name.length;
     };
     // for the following statement asks the paul object to call a function that was added to the Mammal prototype after paul was constructed.
-    equals(paul.numberOfLettersInName(), __, "how long is Paul's name?");
+    equals(paul.numberOfLettersInName(), 4, "how long is Paul's name?");
 });
 
 // helper function for inheritance. 
@@ -55,6 +55,6 @@ extend(Bat, Mammal);
 
 test("Inheritance", function() {
     var lenny = new Bat("Lenny", "1.5m");
-    equals(lenny.sayHi(), __, "what does Lenny say?");
-    equals(lenny.wingspan, __, "what is Lenny's wingspan?");
+    equals(lenny.sayHi(), "Hello, my name is Lenny", "what does Lenny say?");
+    equals(lenny.wingspan, "1.5m", "what is Lenny's wingspan?");
 });
